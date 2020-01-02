dd067a24c31377e968419c39580c6ee69426ae18
hyunseok <mrhyunseok@naver.com>
2020-01-02 20:40:15 +0900

    Member mvc 패턴 초기세팅

---
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.example.demo.service.MemberService;
+
+@Controller
+public class MemberController {
+	@Autowired
+	private MemberService memberService;
+	
+
+}
